f329710623b98e49fcb8d8d08fd739f13d1f3aa4
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-07-12 21:37:09 +0530

    Update Log.js

--- backend/models/Log.js
+++ backend/models/Log.js
@@ -3,8 +3,26 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Log = sequelize.define('Log', {
-  droneId: DataTypes.STRING,
-  event: DataTypes.STRING,
+  droneId: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  event: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  type: {
+    type: DataTypes.STRING,
+    defaultValue: 'info', // info, warning, error
+  },
+  createdBy: {
+    type: DataTypes.STRING, // can store email or role
+    allowNull: true
+  },
+  source: {
+    type: DataTypes.STRING, // which part generated it (e.g. 'missionController')
+    allowNull: true
+  },
   timestamp: {
     type: DataTypes.DATE,
     defaultValue: DataTypes.NOW
