ed8233e22a9f9d5422fe891589cacbaaa68533fe
Jasmin Jamadar <jasmin.jamadar23@tnu.in>
2025-07-12 21:28:56 +0530

    Update droneController.js

--- backend/controllers/droneController.js
+++ backend/controllers/droneController.js
@@ -3,13 +3,25 @@ const { Drone } = require('../models');
 // Create a new drone
 exports.createDrone = async (req, res) => {
   try {
-    const { droneId, status, gps, battery } = req.body;
-    const drone = await Drone.create({ droneId, status, gps, battery });
+    const { droneId, model, status, battery, gps_location } = req.body;
+
+    const drone = await Drone.create({
+      droneId,
+      model,
+      status,
+      battery,
+      gps: gps_location  // no stringify
+    });
+
     res.status(201).json(drone);
-  } catch (err) {
+  } catch (error) {
+    console.error('Drone creation error:', error);
     res.status(500).json({ error: 'Failed to create drone' });
   }
 };
+
+
+
 
 // Get all drones
 exports.getDrones = async (req, res) => {
